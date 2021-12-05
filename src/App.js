beb0d1105ee6ee4c6f7a32e027083de212e1d1dd
Hamza Waseer <hamzawaseer1@gmail.com>
2021-12-05 21:27:19 +0500

    initial slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Nav from "./components/Navbar"
+import Slider from "./components/Slider"
 
 function App() {
   return (
     <>
     <Nav />
+    <Slider />
     </>
   );
 }
